812eda801cbd9986dd4bd07de6ac805b500d8e9a
Vid Jercic <vid.jercic@gmail.com>
2015-04-16 10:41:34 +0200

    a

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,56 @@
+class PostsController < ApplicationController
+	before_action :authenticate_user!, except: [:index, :show]
+
+	def index
+		@posts = Post.all.order('created_at DESC')
+	end
+
+	def new
+		@post = Post.new
+
+	end
+
+	def create
+		@post = Post.create(post_params)
+		
+		if @post.save
+			redirect_to @post
+		else
+			render "new"
+		end	
+
+	end
+
+	def show
+		@post = Post.find(params[:id])
+	end
+
+	def edit
+		@post = Post.find(params[:id])
+	end
+
+	def update
+		@post = Post.find(params[:id])
+
+		if @post.update(params[:post].permit(:title, :body, :image))
+		 	redirect_to @post
+		else
+			render 'edit'
+		 end 
+	end
+
+	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
+
+		redirect_to root_path
+	end
+
+	private
+		def post_params
+			params.require(:post).permit(:title, :body, :image)
+		end
+
+
+
+end
---
+++ app/models/post.rb
@@ -0,0 +1,11 @@
+class Post < ActiveRecord::Base
+	has_many :comments, dependent: :destroy
+	validates :title, presence: true, length: {minimum: 5}
+	validates :body, presence: true
+	validates :image, presence: true
+
+	
+	has_attached_file :image, styles: { large: "600x600>", medium: "300x300>", thumb: "150x150#" }
+	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+
+end
