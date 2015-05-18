05a10365f24c3dcab6dcf8676930bb1e9b2e9843
Vid Jercic <vid.jercic@gmail.com>
2015-05-18 09:47:36 +0200

    fuknu vn od paperclipa sraje

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,12 +2,10 @@ class Post < ActiveRecord::Base
 	has_many :comments, dependent: :destroy
 	validates :title, presence: true, length: {minimum: 5}
 	validates :body, presence: true
-	validates :image, presence: true
+
 
     mount_uploader :picture, PictureUploader
 
-	
-	has_attached_file :image, styles: { large: "600x600>", medium: "300x300>", thumb: "150x150#" }
-	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+
 
 end
