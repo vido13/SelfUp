a0d478e05cd645a3b262d31dd6b2af9085d64f17
Vid Jercic <vid.jercic@gmail.com>
2015-05-18 09:54:02 +0200

    nazaj validate za picture

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ActiveRecord::Base
 	has_many :comments, dependent: :destroy
 	validates :title, presence: true, length: {minimum: 5}
 	validates :body, presence: true
+     validates :image, presence: true
 
 
     mount_uploader :picture, PictureUploader
